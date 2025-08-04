86b9ffd1e302b2df5cebce32763a2b1c9e49f9f5
Odysseus <brilliantbatman81@gmail.com>
2025-08-04 21:10:57 +0500

    Homework5 MVVM

--- app/src/main/java/com/mynewproject/dto/Post.kt
+++ app/src/main/java/com/mynewproject/dto/Post.kt
@@ -5,8 +5,8 @@ data class Post(
     val author: String,
     val published: String,
     val content: String,
-    var likes: Int = 0,
-    var likedByMe: Boolean = false,
-    var shares: Int = 0,
-    var sharedByMe: Boolean = false,
+    val likes: Int = 0,
+    val likedByMe: Boolean = false,
+    val shares: Int = 0,
+    val sharedByMe: Boolean = false,
 )
