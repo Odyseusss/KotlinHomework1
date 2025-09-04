77461d4a572d95954b12dc3802431c15638454dd
Odysseus <brilliantbatman81@gmail.com>
2025-09-04 21:53:49 +0500

    CRUD Homework

--- app/src/main/java/com/mynewproject/activity/MainActivity.kt
+++ app/src/main/java/com/mynewproject/activity/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mynewproject.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.mynewproject.R
+import com.mynewproject.adapter.OnInteractionListener
 import com.mynewproject.adapter.PostAdapter
 import com.mynewproject.databinding.ActivityMainBinding
+import com.mynewproject.dto.Post
+import com.mynewproject.util.AndroidUtils
 import com.mynewproject.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +21,63 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: PostViewModel by viewModels()
 
-        val adapter = PostAdapter({
-            viewModel.like(it.id)
-        }, {
-            viewModel.share(it.id)
+        val adapter = PostAdapter(object : OnInteractionListener {
+            override fun like(post: Post) {
+                viewModel.like(post.id)
+            }
+
+            override fun share(post: Post) {
+                viewModel.share(post.id)
+            }
+
+            override fun remove(post: Post) {
+                viewModel.remove(post.id)
+            }
+
+            override fun edit(post: Post) {
+                viewModel.edit(post)
+            }
+
         })
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
+            val new = posts.size > adapter.currentList.size && adapter.currentList.isNotEmpty()
+            adapter.submitList(posts) {
+                if (new) {
+                    binding.list.smoothScrollToPosition(0)
+                }
+            }
+        }
+
+        viewModel.edited.observe(this) {
+            if (it.id != 0L) {
+                binding.content.setText(it.content)
+                binding.editGroup.visibility = View.VISIBLE
+                AndroidUtils.showKeyboard(binding.content)
+            } else {
+                binding.editGroup.visibility = View.GONE
+                binding.content.setText("")
+                binding.content.clearFocus()
+                AndroidUtils.hideKeyboard(binding.content)
+            }
+        }
+
+        binding.deleteEditButton.setOnClickListener {
+            viewModel.cancelEdit()
+        }
+
+        binding.save.setOnClickListener {
+            val text = binding.content.text.toString()
+            if (text.isBlank()) {
+                Toast.makeText(this@MainActivity, R.string.error_empty_content, Toast.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            }
+            viewModel.save(text)
+
+            binding.content.setText("")
+            binding.content.clearFocus()
+            AndroidUtils.hideKeyboard(binding.content)
         }
     }
 }
--- app/src/main/java/com/mynewproject/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/mynewproject/viewmodel/PostViewModel.kt
@@ -1,16 +1,45 @@
 package com.mynewproject.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mynewproject.dto.Post
 import com.mynewproject.repository.PostRepository
 import com.mynewproject.repository.PostRepositoryInMemory
+import kotlin.String
+
+private val empty = Post(
+    id = 0,
+    author = "",
+    published = "",
+    content = "",
+    likes = 0,
+    likedByMe = false,
+    shares = 0,
+    sharedByMe = false,
+)
 
 class PostViewModel: ViewModel() {
 
     private val repository: PostRepository = PostRepositoryInMemory()
     val data: LiveData<List<Post>> = repository.get()
+    val edited = MutableLiveData(empty)
     fun like(id: Long) = repository.likeById(id)
     fun share(id: Long) = repository.shareById(id)
-
+    fun remove(id: Long) = repository.removeById(id)
+    fun save(text: String) {
+        edited.value?.let {
+            val content = text.trim()
+            if (content != it.content) {
+                repository.save(it.copy(content = content))
+            }
+        }
+        edited.value = empty
+    }
+    fun edit(post: Post) {
+        edited.value = post
+    }
+    fun cancelEdit() {
+        edited.value = empty
+    }
 }
